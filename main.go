38fd9b74b0c60acca19ac8ef96efb5393b7ca96e
Ksenatos <naboychenko.p@gmail.com>
2016-09-28 00:01:01 +0300

    оптимизировал. Повыносил многое в отдельные функции. Ненужное удалил

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
-//	"io/ioutil"
 	"os"
-//	"strings"
+	"bufio"
 )
-var dic []byte
-var dic_2 [][]byte
-
 //это для обработки ошибок
 //она будет юзаться после каждого раза, где будет заполняться err
 //логической нагрузки не несет
@@ -18,48 +14,44 @@ func check(e error) {
 			log.Fatal(e)
 			}
 }
+//подготавливает файл
+//и еще возвращает инфу о файле в переменной stat (мне нужна была для того что бы узнать длинну файла)
+func work_with_files(path string) (file *os.File, stat os.FileInfo){
+	file, err := os.Open(path);
+	check(err)
+	// defer file.Close()
+	stat, err = file.Stat()
+	check(err)
+	return file, stat
+}
 
 func main() {
-	// fin, err := os.Open("input.txt");
-	// check(err)
-	// defer fin.Close()
-
-//	fout, err := os.Open("output.txt");
-//	check(err)
-//	defer fout.Close()
-
-//заполняем массив уникальных байтов (первое считывание)
-	dic = fill_in_dic(dic)
-	fmt.Println(dic)
-//по сути это dic_2 = dic
-//dic_2 - двумерный массив, и приходится юзать спец функцию чтобы заполнить его
-//dic_2 - будет основным словарем где будут накопляться сочетания байтов
-	dic_2 = add_onechar_lines(dic)
-	fmt.Println("dictionary=  ", dic_2)
-//тут будет собственно закодированый текст
-	var resultatik []int
-	resultatik = compress(dic_2)
-	fmt.Println("message=  ", resultatik)
-
-//	str := string(dic)
-// twoD := make([][]int, 3)
-//var twoD [][]int
-
-
-
+	var dict [][]byte
+	//заполняем словарь единичными элементами
+	dict = fill_in_dbl_dic(dict)
+	fmt.Println("dictionary=  ", dict)
+	//вызываем compress
+	//тут будет собственно закодированый текст
+	message := compress(dict)
+	fmt.Println("message=  ", message)
+	//это вывод в файл, он безобразный, я потом поправлю
+	fout, err := os.Create("output.txt");
+	check(err)
+	w := bufio.NewWriter(fout)
+	fmt.Fprintln(w, message)
+	w.Flush()
 }
 
-func byte_in_slice(arr []byte, ch byte) bool{
-	var result = false
-//	var id = 0
-	for i := range arr {
-		if arr[i] == ch {
-			result = true
-			break
-		}
-	}
-	return result
-}
+// func byte_in_slice(arr []byte, ch byte) bool{
+// 	var result = false
+// 	for i := range arr {
+// 		if arr[i] == ch {
+// 			result = true
+// 			break
+// 		}
+// 	}
+// 	return result
+// }
 //Тут проыеряется есть ли в словаре arr "символ" ch, и если есть, то возвращается еще позиция этого символа в массиве
 func byte_in_dbl_slice(arr [][]byte, ch []byte) (bool, int){
 	var result = false
@@ -83,37 +75,52 @@ func byte_in_dbl_slice(arr [][]byte, ch []byte) (bool, int){
 }
 
 //тоже самое что и byte_in_dbl_slice только для одномерного массива
-func byte_id_slice(arr []byte, ch byte) int{
-	var result = 0
-//	var id = 0
-	for i := range arr {
-		if arr[i] == ch {
-			result = i
-			break
-		}
-	}
-	return result
-}
+// func byte_id_slice(arr []byte, ch byte) int{
+// 	var result = 0
+// 	for i := range arr {
+// 		if arr[i] == ch {
+// 			result = i
+// 			break
+// 		}
+// 	}
+// 	return result
+// }
+// func fill_in_dic(dict []byte) []byte {
+// 	char := make([]byte, 1)
+// 	fin, err := os.Open("input.txt");
+// 	check(err)
+// 	defer fin.Close()
+// 	stat, err := fin.Stat()
+// 	check(err)
+// 	for i := 0; i < int(stat.Size()); i++ {
+// 		_, err := fin.Read(char)
+// 		check(err)
+// 		fmt.Println(" bl ", byte_in_slice(dict, char[0]), " dict ", dict)
+// 		if !byte_in_slice(dict, char[0]){
+// 			dict = append(dict, char[0])
+// 		}
+// 	}
+// 	return dict
+// }
 //заполняем массив уникальных байтов
-func fill_in_dic(dict []byte) []byte {
+func fill_in_dbl_dic(dict [][]byte) [][]byte {
 	char := make([]byte, 1)
-	fin, err := os.Open("input.txt");
-	check(err)
-	defer fin.Close()
-	stat, err := fin.Stat()
-	check(err)
-	for i := 0; i < int(stat.Size()); i++ {
+	fin, stat := work_with_files("input.txt")
+	 for i := 0; i < int(stat.Size()); i++ {
 		_, err := fin.Read(char)
 		check(err)
-		if !byte_in_slice(dict, char[0]){
-			dict = append(dict, char[0])
+		bl, _ := byte_in_dbl_slice(dict, char)
+		if !bl{
+			line := make([]byte, len(char))
+			copy(line, char)
+			dict = append(dict, line)
 		}
 	}
 	return dict
 }
 //ГЛАВНЫЙ ДВИЖ
 func compress(dict [][]byte) (message []int) {
-	//искусственный Cchar
+	//искусственный char
 	//нужен что бы считывать посимвольно
 	char := make([]byte, 1)
 	next_char := make([]byte, 1)
@@ -122,22 +129,15 @@ func compress(dict [][]byte) (message []int) {
 	var hlp_line []byte
 	var id int
 	var bl bool
-	//открываем файл
-	fin, err := os.Open("input.txt");
-	check(err)
-	//закрываем файл. в го можно его закрыть заранее)))
-	defer fin.Close()
-	//узнаем длинну файла - это вместо EOF
-	stat, err := fin.Stat()
-	check(err)
+	fin, stat := work_with_files("input.txt")
 	//считываем первый символ в файле
-	_, err = fin.Read(next_char)
+	_, err := fin.Read(next_char)
 	check(err)
 	//забиваем в поточную строку первый символ
 	curent_line = append(curent_line, next_char[0])
 	//главный цыкл. пока не считался весь файл
 	for i := 1; i < int(stat.Size()); i++ {
-		fmt.Println("------------------------------")
+		fmt.Println("-----------------------------------------------------------")
 		_, err := fin.Read(char)
 		check(err)
 		hlp_line = append(curent_line, char[0])
@@ -168,10 +168,10 @@ func compress(dict [][]byte) (message []int) {
 }
 //позволяет присвоить двумерному массиву одномерный
 //например есть dict [1 2 3 4 5], тогда res заполниться так [[1] [2] [3] [4] [5]]
-func add_onechar_lines (dict []byte) (res [][]byte) {
-	for i := range dict {
-		res = append(res, []byte{})
-		res[i] = append(res[i], dict[i])
-	}
-	return res
-}
+// func add_onechar_lines (dict []byte) (res [][]byte) {
+// 	for i := range dict {
+// 		res = append(res, []byte{})
+// 		res[i] = append(res[i], dict[i])
+// 	}
+// 	return res
+// }
